98a2fd0e01b053a2e005fa33bdcc326e351fa77a
YUVRAJSINGHOO7 <yuvrajsingh2k2@gmail.com>
2025-04-03 20:01:15 +0530

    update ui

--- frontend/src/components/HomePageNav/HomePageNav.jsx
+++ frontend/src/components/HomePageNav/HomePageNav.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Navbar2 from "../Navbar/Navbar2";
 import Geolocation from "../Geolocation/Geolocation";
-import "./HomePageNav.css"; // Import CSS file
+import "./HomePageNav.css";
 
 function HomePageNav() {
   return (
     <div className="homepage-container">
       <Navbar2 />
-      {/*<Geolocation / >*/}
+      <Geolocation />
     </div>
   );
 }
