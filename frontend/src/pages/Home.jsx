900122329ecec4b40d7c59927e82cc6ad82b6a57
YUVRAJSINGHOO7 <yuvrajsingh2k2@gmail.com>
2025-04-08 20:11:48 +0530

    hotel_functionality_added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Hero from "../components/Home/Hero";
 import RecentlyAdded from "../components/Home/RecentlyAdded";
 import Chatbot from "../components/Chatbot/Chatbot";
@@ -6,12 +6,37 @@ import HomePageNav from "../components/HomePageNav/HomePageNav";
 import Hotel from "../components/hotels/hotel";
 
 const Home = () => {
+  const [city, setCity] = useState(null);
+
+  useEffect(() => {
+    if (!navigator.geolocation) return;
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+
+      fetch(
+        `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=en`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setCity(
+            data.city ||
+              data.locality ||
+              data.principalSubdivision ||
+              "Unknown Location"
+          );
+        })
+        .catch(() => setCity("Unknown Location"));
+    });
+  }, []);
+
   return (
     <>
-      <HomePageNav/>
+      <HomePageNav />
       <div className="text-white px-10 py-8">
         <Hero />
-        <Hotel/>
+        <Hotel city={city} />
         <RecentlyAdded />
         <Chatbot />
       </div>
